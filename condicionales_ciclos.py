e7e87420dd827b2ee2a05730d868425371cb48fe
TORRES DE AVILA LUIS <ltorres49@cuc.edu.co>
2022-03-09 20:31:14 -0500

    Commit Inicial

---
+++ condicionales_ciclos.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# Tabla de la verdad
+
+# Tabla de verdad and
+print(True and True)
+print(True and False)
+print(False and False)
+print(False and False)
+
+# Tabla de verdad or
+print(True and True)
+print(True and False)
+print(False and False)
+print(False and False)
+
+# Negacion
+
+print(not True)
+print(not False)
+
+print(True and False and True )
+
+print(True and (False and True)
+      or False or (True or True)
+      )
+
+# Estructura if
+
+x = 6
+if (x > 0):
+    print('1')
+else:
+    print('2')
+print('3')
+
+
+if x > 0:
+    print('Mayor que cero')
+elif x == 0:
+    print('Es igual a cero')
+else:
+    print('Entro aqui')
+    
+
+# dada la edad de una persona diga si es mayor o menor de edad
+
+edad = int(input('Ingrese su edad: \n'))
+
+if edad >= 18:
+    print('Es mayor de edad')
+else:
+    print('Es menor de edad')
+    
+
+# 
+
+calificacion = float(input('Ingrese la calificacion del estudiante: \n'))
+
+if calificacion < 0 or calificacion > 5:
+    print('No es una nota valida')
+elif calificacion >= 3:
+    print('Aprobo')
+else:
+    print('Reprobo')
+    
+# 
+
+numero = int(input('ingrese un numero \n'))
+
+if numero < 0:
+    print('Es un numero negativo')
+elif numero == 0:
+    print('El numero es cero')
+else:
+    print('Es un numero positivo')
+
+
+    
+ # Ciclos 
+
+# Range - Iterador Flojo
+
+for valor in range(4):
+    print(valor)
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
