5e1c6dda1cd18266c603b1a13167f4169d71c89e
TORRES DE AVILA LUIS <ltorres49@cuc.edu.co>
2022-03-09 20:56:19 -0500

    Ciclos y Condicionales

--- condicionales_ciclos.py
+++ condicionales_ciclos.py
@@ -89,11 +89,49 @@ else:
 for valor in range(4):
     print(valor)
    
+
+for valor in range(1, 6):
+    print(valor)
     
+
+for valor in range(1, 100, 2):
+    print(valor)
     
-    
-    
-    
+ 
+for i in range(1, 11):
+    for j in range(1, 6):
+        print(i,j)  
+        
+# While
+
+while True:
+    print('Hola Mundo')
+    break
+
+#
+
+notas = 0
+numero_notas = int(input('Ingrese el numero de notas'))    
+for i in range(1, numero_notas + 1):
+    while True:
+        nota = float(input(f'Ingrese la nota numero {i}'))
+        if nota <5 and nota >0:
+            break
+        notas = notas + nota
+promedio = notas / numero_notas
+print(f'El promedio dinal de las notas {numero_notas} notas es:', promedio)
+
+# Tipos de colecciones
+
+# Listas o vectores
+# Tipo de dato mutable y ordenado
+
+a = [2, 3, 4]
+b = [2, True, 'Hola', 3.4]
+c = [2, [True, False], ['Hola', 'Mundo'], [2.3, 3.4, 4.6, 7.8]]
+
+for var in c:
+    print(var)
     
     
     
